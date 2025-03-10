0a7a492b51a206e198987c3445c562b15171ed76
bgarza9188 <bgarza9188@gmail.com>
2025-03-10 00:48:35 -0500

    Updated notes, and main file
    This adds repeated typing, window title, and a swim animation.

--- TypeyTheWhale/main.lua
+++ TypeyTheWhale/main.lua
@@ -1,45 +1,55 @@
 local utf8 = require("utf8")
+local whaleCenterX = 157
+local whaleCenterY = 116
+local rateOfSwim = 35
 
 function love.load()
+	love.window.setTitle("Typey the Whale")
 	textXPosition = 0
-	randomWhaleXPosition = 0
-	randomWhaleYPosition = 25 
+	centerX = love.graphics.getWidth() / 2
+	centerY = love.graphics.getHeight() / 2
+	currentWhaleXPosition = centerX - whaleCenterX -- minus because the window starts at 0,0 in the top left
+	currentWhaleYPosition = centerY - whaleCenterY
     whale = love.graphics.newImage("whale.png")
-    text = "Type away! -- "
 	textColorBlack = {0, 0, 0}
-    -- enable key repeat so backspace can be held down to trigger love.keypressed multiple times.
-    love.keyboard.setKeyRepeat(true)
 	local font = love.graphics.getFont()
-	coloredText = love.graphics.newText(font, {textColorBlack, text})
-end
-
-function love.draw()
-    -- love.graphics.printf(text, 0, 0, love.graphics.getWidth())
-	love.graphics.draw(coloredText, 10, 10)
-    love.graphics.draw(whale, randomWhaleXPosition, randomWhaleYPosition)
-	local r, g, b = love.math.colorFromBytes(132, 193, 238)
+	promptText = love.graphics.newText(font, {textColorBlack, "Help Typey the Whale swim as fast as you can type! Press 'Space' to Start!"})
+	coloredText = love.graphics.newText(font, {textColorBlack, "..."})
+	local r, g, b = love.math.colorFromBytes(132, 193, 238) -- light blue
 	love.graphics.setBackgroundColor(r, g, b)
 end
 
+function love.draw()
+	love.graphics.draw(promptText, 10, 10)
+	love.graphics.draw(coloredText, 10, 30)
+    love.graphics.draw(whale, currentWhaleXPosition, currentWhaleYPosition)
+end
+
 function love.textinput(t)
-	randomWhaleXPosition = math.random(love.graphics.getWidth())
-	randomWhaleYPosition = math.random(love.graphics.getHeight())
 	textXPosition = textXPosition + coloredText:getWidth()
-    coloredText:add({textColorBlack, t}, textXPosition)
-	text = text .. t
+	if t ~= " " then
+		coloredText:add({textColorBlack, t}, textXPosition)
+	end
+end
+
+function love.update(dt)
+   if love.keyboard.isDown("space") then
+		-- move the whale back into frame if it gets too close to edge of window
+		if currentWhaleXPosition > love.graphics.getWidth() or currentWhaleXPosition < 0 then
+			currentWhaleXPosition = whaleCenterX
+		end
+		if currentWhaleYPosition > love.graphics.getHeight() or currentWhaleYPosition < 0 then
+			currentWhaleYPosition = whaleCenterY
+		end
+		-- calculate a new postion for the whale to move to, move by 25 pixels each update
+		currentWhaleXPosition = currentWhaleXPosition + ((25 * math.random(-1, 1)) * (rateOfSwim * dt))
+		currentWhaleYPosition = currentWhaleYPosition + ((25 * math.random(-1, 1)) * (rateOfSwim * dt))
+   end
 end
 
 function love.keypressed(key)
-    if key == "backspace" then
-        -- get the byte offset to the last UTF-8 character in the string.
-        local byteoffset = utf8.offset(text, -1)
-
-        if byteoffset then
-            -- remove the last UTF-8 character.
-            -- string.sub operates on bytes rather than UTF-8 characters, so we couldn't do string.sub(text, 1, -2).
-            text = string.sub(text, 1, byteoffset - 1)
-        end
-		coloredText:set({textColorBlack, text})
-		textXPosition = 0
+    if key == "space" then
+	   textXPosition = 0
+	   coloredText:set({textColorBlack, ""})
     end
 end
